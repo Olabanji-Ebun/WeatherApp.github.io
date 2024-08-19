c823e406b380bb0b2d00507f2a325879adfef290
Olabanji-Ebun <ayooluwaebun8@gmail.com>
2024-08-19 23:52:49 +0100

    Made Changes

--- assets/js/route.js
+++ assets/js/route.js
@@ -1,6 +1,27 @@
-/**
- * @license MIT
- * @fileoverview Menage all routes
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
+'use strict';
+
+import { updateWeather, error404 } from "../js/app.js";
+const defaultLocation = "#/weather?lat=51.5073219&lon=0.1276474" // London
+
+const currentLocation = function () {
+
+}
+
+const searchedLocation = query => {
+
+}
+
+const routes = new Map([
+    ["*/current-location", currentLocation],
+    ["*/weather", searchedLocation]
+]);
+
+const checkHash = function () {
+
+}
+
+window.addEventListener("hashchange", checkHash);
+
+window.addEventListener("load", function () {
+    
+});
